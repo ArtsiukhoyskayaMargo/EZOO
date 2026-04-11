952f34178c348b06ddf30e7d086ac2029bd945c4
Andrei Artsiukouski <andrei.artsiukouski@zextras.com>
2026-04-11 18:58:40 +0200

    05

--- tests/catsDryFood.spec.ts
+++ tests/catsDryFood.spec.ts
@@ -55,10 +55,9 @@ test('03 Verify multiple items in the cart', async ({ pageManager }) => {
     await pageManager.catalogPage.isFilteredBy(uiName);
   });
 
-  test('05 The filter was applied part 2', async ({ pageManager }) => {
+  test('05 The filter was applied - intentional fail', async ({ pageManager }) => {
   await pageManager.catalogPage.filterBy(brands.royalCanin.filterName);
   await pageManager.catalogPage.isFilteredBy(brands.royalCanin.uiName);
-  
   // специально падаем
   await expect(pageManager.catalogPage.cartButton).toHaveText('это точно не текст корзины');
 });
